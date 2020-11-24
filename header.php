4bec84bcd9db1594fbd69c24ebd3e74b0f0d9dde
unknown <aldoj03@gmail.com>
2020-11-24 14:44:29 -0400

    arreglos

--- header.php
+++ header.php
@@ -6,10 +6,11 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170778031-1"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-170778031-1');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-170778031-1');
 </script>
 <?php wp_head(); ?>
 </head>
